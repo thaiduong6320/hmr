6e88ecbc6c25191406305e00c663e6600002712a
BlueD <thaiduong6320@gmail.com>
2023-07-04 16:27:00 +0700

    update

--- hmr_description/src/pose.py
+++ hmr_description/src/pose.py
@@ -1,76 +1,69 @@
-// #include "ros/ros.h"
-// #include "std_msgs/Float64.h"
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <cctype>
-#include <cstring>
-#include "unistd.h"
-#include <cassert>
+import csv
+import time
+from std_msgs.msg import Float64
+import rospy
+with open('/home/dev/hmr_ws/src/hmr/hmr_description/src/pose.csv', 'r') as csvfile:
+    spamreader = csvfile.read().split('\n')
+    i = 0
+    for row in spamreader:
+        if i<7 :
+            q_set = row.split(',')
+            f =[]
+            for item in q_set:
+                f.append(Float64(item))
+            lp1 = type(f[0])
+            lp2 = type(f[1])
+            lp3 = type(f[2])
+            rp1 = type(f[3])
+            rp2 = type(f[4])
+            rp3 = type(f[5])
 
-using namespace std;
+            print(lp1)
+            print(lp2)
+            print(lp3)
+            print(rp1)
+            print(rp2)
+            print(rp3)
+            time.sleep(1)
+            i = i + 1
+        if i==7 :
+            time.sleep(5)
+        time.sleep(0.2)
 
-string split(string str, string delimiter){
-    size_t pos = 0;
-    string token;
-    while ((pos = str.find(delimiter)) != string::npos) {
-        token = str.substr(0, pos);
-        cout << token << endl;
-        str.erase(0, pos + delimiter.length());
-    }
-    return str;
-}
 
-int main(int argc, char **argv)
-{
-    string lp1;
-    string lp2;
-    string lp3;
-    string rp1;
-    string rp2;
-    string rp3;
-    string filename("pose.csv");
-    vector<string> lines;
-    string line;
-    // const char *f;
-    // const char *s = ",";
-    //Open file with ifstream
-    ifstream input_file(filename);
-    //Check open file
-    if (!input_file.is_open()) {
-        cerr << "Could not open the file - '"
-            << filename << "'" << endl;
-        return EXIT_FAILURE;
-    }
-    //Read line
-    while (getline(input_file, line, '\n')){
-        lines.push_back(line);//Save line
-    }
-    int i = 0;
-    
-    if(i<7)
-    {
-        // string f(string lines[], ",");
-        string f = {lines[i].c_str()};
-        float k = stof(split(f,","));
-        // float num_float = stof(k);
-        cout << k << endl;
-        i = i + 1;
-    }
-    // for (int i = 0; i < 7; i++)
-    // {   
-    //     // string f(string lines[], ",");
-    //     string f = {lines[i].c_str()};
-    //     float k = stof(split(f,","));
-    //     // float num_float = stof(k);
-    //     cout << k << endl;
-    // }
-    if(i==7)
-    {
-        
-    }
-    input_file.close();
-    return 0;
-}
+# with open('/home/dev/hmr_ws/src/hmr/hmr_description/src/pose.csv', 'r') as csvfile:
+#     spamreader = csv.reader(csvfile)
+#     i = 0
+#     for row in spamreader:
+#         if i<7 :
+#             q_set = row
+#             q0 = (float, q_set[0])
+#             q1 = (float, q_set[1])
+#             q2 = (float, q_set[2])
+#             q3 = (float, q_set[3])
+#             q4 = (float, q_set[4])
+#             q5 = (float, q_set[5])
+#             print(q0)
+#             print(q1)
+#             print(q2)
+#             print(q3)
+#             print(q4)
+#             print(q5)
+#             print('__')
+#             time.sleep(1)
+#             i = i + 1
+#         if i==7 :
+#             time.sleep(5)
+#         time.sleep(0.2)
+
+# with open('/home/dev/hmr_ws/src/hmr/hmr_description/src/pose.csv', 'r') as csvfile:
+#     spamreader = csvfile.read().split('\n')
+#     i = 0
+#     for row in spamreader:
+#         q_set = (float, row.split(','))
+#         q = q_set[1][1]
+#         print(q)
+#         i = i + 1
+#         if i==7 :
+#             time.sleep(5)
+#         time.sleep(0.2)
